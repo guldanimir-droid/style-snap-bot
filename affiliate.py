5c4f1be72726bc9337ca3b2756091ab3d0efd147
guldanimir-droid <guldanimir@gmail.com>
2026-03-27 18:44:34 +0300

    Update affiliate.py

--- affiliate.py
+++ affiliate.py
@@ -3,5 +3,4 @@ import logging
 logger = logging.getLogger(__name__)
 
 def generate_affiliate_links(advice_text: str) -> str:
-    """Возвращает исходный текст без добавления ссылок"""
     return advice_text
