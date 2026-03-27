2e8f57960576866875ee121141ca64e8d8a395ce
guldanimir-droid <guldanimir@gmail.com>
2026-03-27 14:59:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,5 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 DEVELOPER_ID = os.getenv("DEVELOPER_ID")
-# GigaChat credentials
 GIGACHAT_CLIENT_ID = os.getenv("GIGACHAT_CLIENT_ID")
 GIGACHAT_SECRET = os.getenv("GIGACHAT_SECRET")
