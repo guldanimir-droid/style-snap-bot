ab10b7b107fafa5072d89e83442d9ae9498c1d20
guldanimir-droid <guldanimir@gmail.com>
2026-04-01 12:57:55 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,5 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class ProfileStates(StatesGroup):
-    waiting_gender = State()   # ожидаем выбор пола
-    waiting_style = State()    # ожидаем выбор стиля
+    waiting_gender = State()
+    waiting_style = State()
+
+class FavoritesStates(StatesGroup):
+    browsing = State()   # навигация по избранному
