b853450cf1fbe13108436b3a3c2ae66c9be85006
guldanimir-droid <guldanimir@gmail.com>
2026-04-03 07:14:45 +0300

    Update states.py

--- states.py
+++ states.py
@@ -5,10 +5,6 @@ class ProfileStates(StatesGroup):
     waiting_style = State()
 
 class WardrobeStates(StatesGroup):
-    waiting_for_photo = State()       # ожидаем фото
-    waiting_clothing_type = State()   # ожидаем ввод типа одежды
-    waiting_description = State()     # ожидаем ввод описания
-
-class TryOnStates(StatesGroup):
-    waiting_person_photo = State()
-    waiting_clothing_selection = State()
+    waiting_for_photo = State()
+    waiting_clothing_type = State()
+    waiting_description = State()
