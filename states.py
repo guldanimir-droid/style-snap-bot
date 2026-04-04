7e2fc7d1f2e004aa958e62db09103deeae77f407
guldanimir-droid <guldanimir@gmail.com>
2026-04-04 06:47:32 +0300

    Update states.py

--- states.py
+++ states.py
@@ -3,8 +3,3 @@ from aiogram.fsm.state import State, StatesGroup
 class ProfileStates(StatesGroup):
     waiting_gender = State()
     waiting_style = State()
-
-class WardrobeStates(StatesGroup):
-    waiting_for_photo = State()
-    waiting_clothing_type = State()
-    waiting_description = State()
