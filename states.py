040bd3035c0669acf9c8a5a9afb4a79495b4e84e
guldanimir-droid <guldanimir@gmail.com>
2026-04-05 20:31:31 +0300

    Update states.py

--- states.py
+++ states.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 class ProfileStates(StatesGroup):
     waiting_gender = State()
     waiting_style = State()
+    waiting_figure = State()
+    waiting_color = State()
+    waiting_budget = State()
