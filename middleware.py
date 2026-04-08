93fb1525db7017e717650a6825e2343c5d57e092
guldanimir-droid <guldanimir@gmail.com>
2026-04-08 19:37:14 +0300

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -7,7 +7,7 @@ class AntiSpamMiddleware(BaseMiddleware):
     def __init__(self, time_limit: int = 10):
         super().__init__()
         self.user_requests = defaultdict(list)
-        self.time_limit = time_limit # <- минимальное время между запросами (в секундах)
+        self.time_limit = time_limit  # минимальное время между запросами (секунды)
 
     async def __call__(self, handler, event: Update, data: dict):
         # Проверяем, является ли событие сообщением от пользователя
@@ -24,10 +24,12 @@ class AntiSpamMiddleware(BaseMiddleware):
             # Если запросов за последние N секунд уже слишком много
             if len(self.user_requests[user_id]) >= 1:
                 # Отправляем пользователю предупреждение
-                await event.message.answer(f"Пожалуйста, не спешите. Подождите {self.time_limit} секунд перед следующим запросом.")
-                # Прерываем обработку, чтобы бот не тратил ресурсы
+                await event.message.answer(
+                    f"⏳ Пожалуйста, не спешите. Подождите {self.time_limit} секунд перед следующим запросом."
+                )
+                # Прерываем обработку
                 return
             self.user_requests[user_id].append(current_time)
-        
-        # Если всё в порядке, передаём управление дальше, к основному обработчику
+
+        # Если всё в порядке, передаём управление дальше
         return await handler(event, data)
